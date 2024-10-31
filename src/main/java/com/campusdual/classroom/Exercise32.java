e1126687d96421b008e9fe44615615a780790efb
Iris Diaz Feijoo <irisdf_4@hotmail.com>
2024-10-31 16:32:55 +0100

    Terminado ejercicio 32

--- src/main/java/com/campusdual/classroom/Exercise32.java
+++ src/main/java/com/campusdual/classroom/Exercise32.java
@@ -2,33 +2,54 @@ package com.campusdual.classroom;
 
 import com.campusdual.util.Utils;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Exercise32 {
 
     public static void main(String[] args) {
-
+        String textToSave = generateStringToSave(null);
+        try {
+            printToFile(textToSave);
+            System.out.println("Texto guardado en data.txt con éxito.");
+        } catch (Exception e) {
+            System.err.println("Error al guardar el texto en el archivo.");
+            e.printStackTrace();
+        }
     }
+
+
 
     public static String generateStringToSave(String string) {
-
+        if (string == null) {
+            return generateUserInputToSave();
+        }
+        return string;
     }
 
-    private static String generateUserInputToSave(){
+    private static String generateUserInputToSave() {
         StringBuilder sb = new StringBuilder();
         System.out.println("Escribe debajo el texto que quieras. Pulsa \"ENTER\" 2 veces seguidas para finalizar:");
         String string;
-        while(!(string = Utils.string()).isEmpty()){
+        while (!(string = Utils.string()).isEmpty()) {
             sb.append(string).append(System.lineSeparator());
         }
         return sb.toString();
     }
 
     public static void printToFile(String string) {
-
+        File file = new File("src/main/resources/data.txt");
+        file.getParentFile().mkdirs();
+        try (PrintWriter pw = new PrintWriter(new FileWriter(file))) {
+            pw.println(string);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-
-
 }
+
+
+
+
+
